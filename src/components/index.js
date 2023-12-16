a52713ad376765cb10d06135ac0adea4ecc45a6b
CIS3250_Derek <dduong03@uoguelph.ca>
2023-12-16 15:29:40 -0500

    added components and default containers

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as cta} from './cta/cta';
+export {default as Feature} from './feature/Feature';
+export {default as Nav} from './nav/nav';
+
